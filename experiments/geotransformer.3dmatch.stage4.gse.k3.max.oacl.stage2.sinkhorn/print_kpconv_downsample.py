05dd97e629497722192cf64f6b0c780e978c9991
kohei <tsuntsunky717@gmail.com>
2025-06-24 20:45:26 +0900

    あ

--- experiments/geotransformer.3dmatch.stage4.gse.k3.max.oacl.stage2.sinkhorn/print_kpconv_downsample.py
+++ experiments/geotransformer.3dmatch.stage4.gse.k3.max.oacl.stage2.sinkhorn/print_kpconv_downsample.py
@@ -1,20 +1,25 @@
 import numpy as np
 from geotransformer.utils.data import registration_collate_fn_stack_mode
 from config import make_cfg
-from dataset import ThreeDMatchPairDataset
 
 if __name__ == "__main__":
     cfg = make_cfg()
-    dataset = ThreeDMatchPairDataset(
-        cfg.data.dataset_root,
-        'val',
-        point_limit=cfg.test.point_limit,
-        use_augmentation=False,
-    )
-    sample = dataset[0]
+    # デモ用データのパス
+    src_file = "data/demo/src.npy"
+    ref_file = "data/demo/ref.npy"
+    src_points = np.load(src_file)
+    ref_points = np.load(ref_file)
+    src_feats = np.ones((src_points.shape[0], 1), dtype=np.float32)
+    ref_feats = np.ones((ref_points.shape[0], 1), dtype=np.float32)
+    data_dict = {
+        "ref_points": ref_points.astype(np.float32),
+        "src_points": src_points.astype(np.float32),
+        "ref_feats": ref_feats.astype(np.float32),
+        "src_feats": src_feats.astype(np.float32),
+    }
     neighbor_limits = [32] * cfg.backbone.num_stages
     collated = registration_collate_fn_stack_mode(
-        [sample],
+        [data_dict],
         cfg.backbone.num_stages,
         cfg.backbone.init_voxel_size,
         cfg.backbone.init_radius,
