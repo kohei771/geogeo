48276cac8773f8530cd39a3cd7685962b9663189
kohei <tsuntsunky717@gmail.com>
2025-06-29 19:41:56 +0900

    マッチングプロット

--- experiments/newmethod_intensity_dim1/test.py
+++ experiments/newmethod_intensity_dim1/test.py
@@ -3,7 +3,9 @@ import os.path as osp
 import sys
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 from geotransformer.engine import SingleTester
 from geotransformer.utils.common import ensure_dir, get_log_string
@@ -40,6 +42,8 @@ class Tester(SingleTester):
         self.output_dir = osp.join(cfg.feature_dir)
         ensure_dir(self.output_dir)
 
+        self.visualized = False  # 可視化フラグ
+
     def test_step(self, iteration, data_dict):
         output_dict = self.model(data_dict)
         return output_dict
@@ -57,10 +61,48 @@ class Tester(SingleTester):
         message += ', nCorr: {}'.format(output_dict['corr_scores'].shape[0])
         return message
 
+    def plot_registration(self, src, ref, src_aligned=None, title=''):
+        fig = plt.figure(figsize=(12, 6))
+        ax1 = fig.add_subplot(121, projection='3d')
+        ax1.scatter(src[:, 0], src[:, 1], src[:, 2], c='r', s=1, label='src')
+        ax1.scatter(ref[:, 0], ref[:, 1], ref[:, 2], c='b', s=1, label='ref')
+        ax1.set_title('Before Registration')
+        ax1.legend()
+        ax1.axis('off')
+
+        ax2 = fig.add_subplot(122, projection='3d')
+        if src_aligned is not None:
+            ax2.scatter(src_aligned[:, 0], src_aligned[:, 1], src_aligned[:, 2], c='r', s=1, label='src_aligned')
+        ax2.scatter(ref[:, 0], ref[:, 1], ref[:, 2], c='b', s=1, label='ref')
+        ax2.set_title('After Registration')
+        ax2.legend()
+        ax2.axis('off')
+
+        plt.suptitle(title)
+        plt.tight_layout()
+        plt.show()
+
     def after_test_step(self, iteration, data_dict, output_dict, result_dict):
         seq_id = data_dict['seq_id']
         ref_frame = data_dict['ref_frame']
         src_frame = data_dict['src_frame']
+
+        # 可視化: 最初のサンプルのみ
+        if not self.visualized:
+            src = output_dict['src_points']  # (N,3) torch.Tensor or np.ndarray
+            ref = output_dict['ref_points']
+            est = output_dict['estimated_transform']  # (4,4)
+            if hasattr(src, 'cpu'):
+                src = src.cpu().numpy()
+            if hasattr(ref, 'cpu'):
+                ref = ref.cpu().numpy()
+            if hasattr(est, 'cpu'):
+                est = est.cpu().numpy()
+            # src点群に推定変換を適用
+            src_h = np.concatenate([src, np.ones((src.shape[0], 1))], axis=1)  # (N,4)
+            src_aligned = (est @ src_h.T).T[:, :3]
+            self.plot_registration(src, ref, src_aligned, title=f'{seq_id} {src_frame}->{ref_frame}')
+            self.visualized = True
 
         file_name = osp.join(self.output_dir, f'{seq_id}_{src_frame}_{ref_frame}.npz')
         np.savez_compressed(
