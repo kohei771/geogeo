20030403e6e6de4e6cceb4fdde247cca6f07219f
kohei <tsuntsunky717@gmail.com>
2025-06-29 17:46:55 +0900

    　

--- geotransformer/datasets/registration/newmethod_intensity/dataset.py
+++ geotransformer/datasets/registration/newmethod_intensity/dataset.py
@@ -98,11 +98,11 @@ class NewMethodIntensityPairDataset(torch.utils.data.Dataset):
         return ref_points, src_points, transform
 
     def _get_pcd_path(self, pcd_relpath):
-        # pcd_relpath: 'newmethod/00/000011.npy' の場合はそのまま使用
-        # pcd_relpath: '00/000011.npy' の場合は 'newmethod/' を前に付ける
-        if not pcd_relpath.startswith('newmethod/'):
-            pcd_relpath = 'newmethod/' + pcd_relpath
-        return osp.join(self.dataset_root, pcd_relpath)
+        # すでに newmethod/ または newmethod_near/ で始まっていればそのまま
+        if pcd_relpath.startswith('newmethod/') or pcd_relpath.startswith('newmethod_near/'):
+            return osp.join(self.dataset_root, pcd_relpath)
+        # それ以外は従来通り
+        return osp.join(self.dataset_root, 'newmethod', pcd_relpath)
 
     def _load_point_cloud(self, file_name):
         points = np.load(file_name)
